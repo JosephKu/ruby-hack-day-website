8bdbbe7e4ffb509ca27822e4c127440901c11ca8
Joseph Ku <chiehfang.ku@gmail.com>
2011-06-05 19:18:58 +0800

    Create a new Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyHackDayWebsite::Application.config.secret_token = '2715978704083394649de191c51c66beb859a4a7053648af83fc56234adf4f0df0e6e68040c0b604b957aa41f911d4f26611ca9c30f689700ac6f7727b2a650f'
